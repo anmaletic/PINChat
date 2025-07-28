adeb00bce933bf17055f63a437ed4220882e5c11
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2025-07-28 12:28:58 +0200

    Refactor MainView and MainViewModel for improved view management.

--- PINChat.UI/PINChat.UI/ViewModels/MainViewModel.cs
+++ PINChat.UI/PINChat.UI/ViewModels/MainViewModel.cs
@@ -1,10 +1,44 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Avalonia.Controls;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using PINChat.UI.Core.Messages;
+using PINChat.UI.ViewManager.Interfaces;
 
-namespace PINChat.UI.ViewModels
+namespace PINChat.UI.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
 {
-    public partial class MainViewModel : ViewModelBase
+
+    private readonly IViewManager _viewManager;
+
+    [ObservableProperty]
+    private UserControl? _currentView;
+
+    public MainViewModel() : this(null!)
     {
-        [ObservableProperty]
-        private string _greeting = "Welcome to Avalonia!";
+    }
+    
+    public MainViewModel(IViewManager viewManager)
+    {
+        _viewManager = viewManager;
+        
+        StrongReferenceMessenger.Default.Register<ChangeViewMessage>(this, OnChangeViewMessageReceived);
+
+        InitializeDefaultView();
+    }
+    
+    private void InitializeDefaultView()
+    {
+        ShowView(nameof(LoginViewModel));
+    }
+    
+    private void OnChangeViewMessageReceived(object recipient, ChangeViewMessage msg)
+    {
+        ShowView(msg.View);
+    }
+    
+    private void ShowView(string viewName)
+    {
+        CurrentView = _viewManager.GetView(viewName);
     }
 }
