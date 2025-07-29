2d5cc830c4491a8eda5b9fd19de22e7ac0cf21a6
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2025-07-29 09:46:42 +0200

    Add initial ChatView, ChatViewModel, and related XAML layout files.

---
+++ PINChat.UI/PINChat.UI/ViewModels/ChatViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.Input;
+using PINChat.UI.Core.Models;
+
+namespace PINChat.UI.ViewModels;
+
+public partial class ChatViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    private UserModel _user = new();
+    
+    [ObservableProperty]
+    private UserModel _selectedContact = new();
+
+    [ObservableProperty]
+    private string _contact = "";
+
+    public ChatViewModel()
+    {
+        GenerateDemoContacts();
+    }
+
+    partial void OnSelectedContactChanged(UserModel value)
+    {
+    }
+
+    private void GenerateDemoContacts()
+    {
+        User = new UserModel()
+        {
+            FirstName = "Antonio",
+            LastName = "Maletic",
+            DisplayName = "Anmal",
+            Contacts =
+            [
+                new UserModel()
+                {
+                    FirstName = "John",
+                    LastName = "Doe",
+                    DisplayName = "JohnD",
+                },
+                new UserModel()
+                {
+                    FirstName = "Jane",
+                    LastName = "Smith",
+                    DisplayName = "JaneS",
+                },
+                new UserModel()
+                {
+                    FirstName = "Alice",
+                    LastName = "Johnson",
+                    DisplayName = "AliceJ",
+                },
+                new UserModel()
+                {
+                    FirstName = "Bob",
+                    LastName = "Brown",
+                    DisplayName = "BobB",
+                },
+                new UserModel()
+                {
+                    FirstName = "Charlie",
+                    LastName = "Davis",
+                    DisplayName = "CharlieD",
+                },
+            ]
+        };
+    }
+
+}
---
+++ PINChat.UI/PINChat.UI/Views/ChatView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PINChat.UI.Views;
+
+public partial class ChatView : UserControl
+{
+    public ChatView()
+    {
+        InitializeComponent();
+    }
+}
