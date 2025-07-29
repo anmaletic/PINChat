7f0b5c5c8763094f8c097b2e044d4440455ccb01
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2025-07-29 13:36:33 +0200

    Add Boolean to Brush and Horizontal Alignment converters for UI.

---
+++ PINChat.UI.Core/Converters/BooleanToBrushConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace PINChat.UI.Core.Converters;
+
+public class BooleanToBrushConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is bool b)
+        {
+            if (b) return TrueBrush;
+            return FalseBrush;
+        }
+        return null;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+
+    public required Avalonia.Media.IBrush TrueBrush { get; set; }
+    public required Avalonia.Media.IBrush FalseBrush { get; set; }
+}
---
+++ PINChat.UI.Core/Converters/BooleanToHorizontalAlignmentConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using Avalonia.Data.Converters;
+using Avalonia.Layout;
+
+namespace PINChat.UI.Core.Converters;
+
+public class BooleanToHorizontalAlignmentConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is bool b)
+        {
+            if (b) return HorizontalAlignment.Right;
+            return HorizontalAlignment.Left;
+        }
+        return HorizontalAlignment.Left; // Default
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
