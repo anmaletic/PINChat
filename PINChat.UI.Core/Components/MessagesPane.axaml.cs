0e4700654bee381bb5bcca1229eff8b2acb46db0
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2025-07-29 13:36:45 +0200

    Add MessagesPane UserControl and associated logic for message handling.

---
+++ PINChat.UI.Core/Components/MessagesPane.axaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+using Avalonia;
+using Avalonia.Controls;
+using PINChat.UI.Core.Models;
+
+namespace PINChat.UI.Core.Components;
+
+public partial class MessagesPane : UserControl
+{   
+    public static readonly StyledProperty<ObservableCollection<ChatMessageModel>> MessagesProperty =
+        AvaloniaProperty.Register<ContactsPane, ObservableCollection<ChatMessageModel>>(nameof(Messages), []);
+
+    public ObservableCollection<ChatMessageModel> Messages
+    {
+        get => GetValue(MessagesProperty);
+        set => SetValue(MessagesProperty, value);
+    }
+    
+    public MessagesPane()
+    {
+        InitializeComponent();
+    }
+}
