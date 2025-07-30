9509ec48a201074a630ad9d61160dce62db4c359
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2025-07-30 08:45:20 +0200

    Refactor ChatMessageModel to use CommunityToolkit's ObservableObject.

--- PINChat.UI.Core/Models/ChatMessageModel.cs
+++ PINChat.UI.Core/Models/ChatMessageModel.cs
@@ -1,12 +1,20 @@
-﻿namespace PINChat.UI.Core.Models;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 
-public class ChatMessageModel
+namespace PINChat.UI.Core.Models;
+
+public partial class ChatMessageModel : ObservableObject
 {
     public DateTime Timestamp { get; set; } = DateTime.Now;
     public string Sender { get; set; } = "";
     public string Content { get; set; } = "";
     public bool IsSent { get; set; } = false;
-    public bool IsReceived { get; set; } = false;
-    public bool IsRead { get; set; } = false;
-    public bool IsOrigin { get; set; }
+    
+    [ObservableProperty]
+    private bool _isReceived = false;
+    
+    [ObservableProperty]
+    private bool _isRead = false;
+
+    [ObservableProperty]
+    private bool _isOrigin;
 }
