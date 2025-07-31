d6bfa3584013abcd2d238639072ab6816cc256df
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2025-07-31 14:23:42 +0200

    Add properties for Id and IsOrigin while refactoring message flags.

--- PINChat.UI.Core/Models/ChatMessageModel.cs
+++ PINChat.UI.Core/Models/ChatMessageModel.cs
@@ -4,17 +4,19 @@ namespace PINChat.UI.Core.Models;
 
 public partial class ChatMessageModel : ObservableObject
 {
+    public string Id { get; set; }
     public DateTime Timestamp { get; set; } = DateTime.Now;
     public string Sender { get; set; } = "";
     public string Content { get; set; } = "";
-    public bool IsSent { get; set; } = false;
     
     [ObservableProperty]
-    private bool _isReceived = false;
+    private bool _isSent;
     
     [ObservableProperty]
-    private bool _isRead = false;
+    private bool _isReceived;
+    
+    [ObservableProperty]
+    private bool _isRead;
 
-    [ObservableProperty]
-    private bool _isOrigin;
+    public bool IsOrigin { get; set; } 
 }
