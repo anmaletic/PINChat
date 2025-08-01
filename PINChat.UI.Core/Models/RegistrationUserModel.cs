0dcd3d3e4f73407b307a91f4f16d376128ebda65
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2025-08-01 11:47:00 +0200

    Add RegistrationUserModel and RegistrationUserValidator classes.

---
+++ PINChat.UI.Core/Models/RegistrationUserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PINChat.UI.Core.Models;
+
+public class RegistrationUserModel
+{
+    public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
---
+++ PINChat.UI.Core/Validators/RegistrationUserValidator.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using PINChat.UI.Core.Models;
+
+namespace PINChat.UI.Core.Validators;
+
+public class RegistrationUserValidator : AbstractValidator<RegistrationUserModel>
+{
+    public RegistrationUserValidator()
+    {
+        RuleFor(x => x.Username)
+            .NotEmpty()
+            .WithMessage("Username is required.")
+            .MinimumLength(3)
+            .WithMessage("Username must be at least 3 characters long.")
+            .MaximumLength(20)
+            .WithMessage("Username must not exceed 20 characters.");
+        
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .WithMessage("Email is required.")
+            .EmailAddress()
+            .WithMessage("Invalid email format.");
+        
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .WithMessage("Password is required.")
+            .MinimumLength(6)
+            .WithMessage("Password must be at least 6 characters long.")
+            .MaximumLength(50)
+            .WithMessage("Password must not exceed 50 characters.");
+        
+        RuleFor(x => x.ConfirmPassword)
+            .Equal(x => x.Password)
+            .WithMessage("Passwords do not match.");
+    }
+    
+}
