5396029c12f33a171a4124aaa7cb559d26715614
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2025-08-06 12:45:39 +0200

    Add ContentTypeHelper class for determining MIME types by file extension.

---
+++ PINChat.UI.Core/Helpers/ContentTypeHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace PINChat.UI.Core.Helpers;
+
+public static class ContentTypeHelper
+{
+    public static string GetType(string fileName)
+    {
+        var extension = Path.GetExtension(fileName)?.ToLowerInvariant();
+        return extension switch
+        {
+            ".jpg" or ".jpeg" => "image/jpeg",
+            ".png" => "image/png",
+            ".gif" => "image/gif",
+            ".bmp" => "image/bmp",
+            ".webp" => "image/webp",
+            _ => "application/octet-stream", // Default to generic binary
+        };
+    }
+}
