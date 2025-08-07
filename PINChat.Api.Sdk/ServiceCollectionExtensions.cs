542c2f1d19cbf4a7eaa2f2eadecea26b3d5599b4
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2025-08-07 07:39:40 +0200

    Update API URL to point to the production server for deployment.

--- PINChat.Api.Sdk/ServiceCollectionExtensions.cs
+++ PINChat.Api.Sdk/ServiceCollectionExtensions.cs
@@ -4,7 +4,7 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddApiSdk(this IServiceCollection services)
     {
-        var apiUrl = new Uri("https://localhost:7233");
+        var apiUrl = new Uri("https://pinchat-v2-api.anmal.dev");
 
         services
             .AddHttpClient();
