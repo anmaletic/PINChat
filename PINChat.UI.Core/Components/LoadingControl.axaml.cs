91becf3b653faa5984cd0e364811098de380b0ea
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2025-08-07 12:50:13 +0200

    Add LoadingControl user interface and logic for loading state.

---
+++ PINChat.UI.Core/Components/LoadingControl.axaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PINChat.UI.Core.Components;
+
+public partial class LoadingControl : UserControl
+{
+    public static readonly StyledProperty<bool> IsLoadingProperty =
+        AvaloniaProperty.Register<LoadingControl, bool>(nameof(IsLoading));
+
+    public bool IsLoading
+    {
+        get => GetValue(IsLoadingProperty);
+        set => SetValue(IsLoadingProperty, value);
+    }
+    
+    public LoadingControl()
+    {
+        InitializeComponent();
+    }
+}
