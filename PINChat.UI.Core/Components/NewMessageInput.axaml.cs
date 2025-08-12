8f51e9294a5823a10cc9dbe85161cb7fbe646747
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2025-08-12 11:32:21 +0200

    Add NewMessageInput component for improved message handling.

---
+++ PINChat.UI.Core/Components/NewMessageInput.axaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.Markup.Xaml;
+
+namespace PINChat.UI.Core.Components;
+
+public partial class NewMessageInput : UserControl
+{
+    public static readonly StyledProperty<string> MessageContentProperty = AvaloniaProperty.Register<NewMessageInput, string>(
+        nameof(MessageContent), defaultBindingMode:BindingMode.TwoWay);
+
+    public string MessageContent
+    {
+        get => GetValue(MessageContentProperty);
+        set => SetValue(MessageContentProperty, value);
+    }
+
+    public static readonly StyledProperty<ICommand?> HandleKeyDownCommandProperty = AvaloniaProperty.Register<NewMessageInput, ICommand?>(
+        nameof(HandleKeyDownCommand));
+
+    public ICommand? HandleKeyDownCommand
+    {
+        get => GetValue(HandleKeyDownCommandProperty);
+        set => SetValue(HandleKeyDownCommandProperty, value);
+    }
+
+    public static readonly StyledProperty<ICommand?> SelectAndSendImageCommandProperty = AvaloniaProperty.Register<NewMessageInput, ICommand?>(
+        nameof(SelectAndSendImageCommand));
+
+    public ICommand? SelectAndSendImageCommand
+    {
+        get => GetValue(SelectAndSendImageCommandProperty);
+        set => SetValue(SelectAndSendImageCommandProperty, value);
+    }
+
+    public static readonly StyledProperty<ICommand?> SendMessageCommandProperty = AvaloniaProperty.Register<NewMessageInput, ICommand?>(
+        nameof(SendMessageCommand));
+
+    public ICommand? SendMessageCommand
+    {
+        get => GetValue(SendMessageCommandProperty);
+        set => SetValue(SendMessageCommandProperty, value);
+    }
+    
+    public NewMessageInput()
+    {
+        InitializeComponent();
+    }
+}
