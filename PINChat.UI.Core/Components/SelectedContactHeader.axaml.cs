ab63f155686e4a4c2e32e4128c6575477637de9b
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2025-08-12 10:59:44 +0200

    Add SelectedContactHeader component to improve UI structure and clarity.

---
+++ PINChat.UI.Core/Components/SelectedContactHeader.axaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using PINChat.UI.Core.Models;
+
+namespace PINChat.UI.Core.Components;
+
+public partial class SelectedContactHeader : UserControl
+{
+    public static readonly StyledProperty<UserModel> SelectedContactProperty =
+        AvaloniaProperty.Register<SelectedContactHeader, UserModel>(
+            nameof(SelectedContact));
+
+    public UserModel SelectedContact
+    {
+        get => GetValue(SelectedContactProperty);
+        set => SetValue(SelectedContactProperty, value);
+    }
+
+    public static readonly StyledProperty<ICommand?> ExportMessagesCommandProperty =
+        AvaloniaProperty.Register<SelectedContactHeader, ICommand?>(
+            nameof(ExportMessagesCommand));
+
+    public ICommand? ExportMessagesCommand
+    {
+        get => GetValue(ExportMessagesCommandProperty);
+        set => SetValue(ExportMessagesCommandProperty, value);
+    }
+
+    public SelectedContactHeader()
+    {
+        InitializeComponent();
+    }
+}
