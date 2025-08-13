eb1652823fe833fff85ba3ef858970e7d611446b
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2025-08-13 19:55:35 +0200

    Add SmartObservableCollection extensions for UI thread operations.

---
+++ PINChat.UI.Core/Extensions/SmartObservableCollectionUiThreadExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Avalonia.Threading;
+
+namespace PINChat.UI.Core.Extensions;
+
+public static class SmartCollectionUiThreadExtensions
+{
+    public static UiThreadSmartCollectionWrapper<T> OnUIThread<T>(
+        this SmartObservableCollection<T> collection)
+    {
+        return new UiThreadSmartCollectionWrapper<T>(collection);
+    }
+}
+
+public class UiThreadSmartCollectionWrapper<T>
+{
+    private readonly SmartObservableCollection<T> _collection;
+
+    internal UiThreadSmartCollectionWrapper(SmartObservableCollection<T> collection)
+    {
+        _collection = collection ?? throw new ArgumentNullException(nameof(collection));
+    }
+
+    public async Task AddRangeAsync(IEnumerable<T> items)
+    {
+        ArgumentNullException.ThrowIfNull(items);
+
+        await Dispatcher.UIThread.InvokeAsync(() => _collection.AddRange(items));
+    }
+
+    public async Task ClearAsync()
+    {
+        await Dispatcher.UIThread.InvokeAsync(() => _collection.Clear());
+    }
+}
