89ab49485cbfb3c55e145e5b0229707863e8861b
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2025-08-14 11:00:24 +0200

    Add AvatarImage component for improved avatar handling in UI.

---
+++ PINChat.UI.Core/Components/AvatarImage.axaml.cs
@@ -0,0 +1,49 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace PINChat.UI.Core.Components;
+
+public partial class AvatarImage : UserControl
+{
+    public static readonly StyledProperty<IImage?> AvatarSourceProperty = AvaloniaProperty.Register<AvatarImage, IImage?>(
+        nameof(AvatarSource));
+
+    public IImage? AvatarSource
+    {
+        get => GetValue(AvatarSourceProperty);
+        set => SetValue(AvatarSourceProperty, value);
+    }
+
+    public static readonly StyledProperty<double> SizeProperty = AvaloniaProperty.Register<AvatarImage, double>(
+        nameof(Size));
+
+    public double Size
+    {
+        get => GetValue(SizeProperty);
+        set => SetValue(SizeProperty, value);
+    }
+
+    public static readonly StyledProperty<double> RadiusProperty = AvaloniaProperty.Register<AvatarImage, double>(
+        nameof(Radius));
+
+    public double Radius
+    {
+        get => GetValue(RadiusProperty);
+        set => SetValue(RadiusProperty, value);
+    }
+
+    public static readonly StyledProperty<Rect> RectProperty = AvaloniaProperty.Register<AvatarImage, Rect>(
+        nameof(Rect));
+
+    public Rect Rect
+    {
+        get => GetValue(RectProperty);
+        set => SetValue(RectProperty, value);
+    }
+
+    public AvatarImage()
+    {
+        InitializeComponent();
+    }
+}
